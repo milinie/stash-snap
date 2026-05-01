58b45743062c01f0bade86cd0bdc2e6119e29c72
milinie <milinie@craftingdreamsfabric.com>
2026-05-01 13:29:30 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
 export default function App() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Stash Snap is Working 🎉</h1>
-      <p>Your Vercel app is live.</p>
-    </div>
-  );
-}
+  return <h1>Hello world</h1>;
 }
